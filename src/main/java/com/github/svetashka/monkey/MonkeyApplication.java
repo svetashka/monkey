d7c388a7669cecef15f30c2d6c77b0689be41d13
Svetlana Koroleva <svetashka@icloud.com>
2019-04-22 12:27:33 +0200

    added maven project

---
+++ src/main/java/com/github/svetashka/monkey/MonkeyApplication.java
@@ -0,0 +1,13 @@
+package com.github.svetashka.monkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonkeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonkeyApplication.class, args);
+	}
+
+}
