829573ae3c78281e10f420e7e9d899e72b793a25
Svetlana Koroleva <svetashka@icloud.com>
2019-05-04 22:15:03 +0200

    create SHA1 from torrent info

--- src/main/java/com/github/svetashka/monkey/MonkeyApplication.java
+++ src/main/java/com/github/svetashka/monkey/MonkeyApplication.java
@@ -3,6 +3,7 @@ package com.github.svetashka.monkey;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.security.MessageDigest;
 
 import java.io.FileInputStream;
 
@@ -17,10 +18,24 @@ public class MonkeyApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         String fileName = args[0];
         System.out.println("File name: " + fileName);
+
         FileInputStream fin = new FileInputStream(fileName);
         byte[] buffer = fin.readAllBytes();
+
         ParseBencode parser = new ParseBencode(buffer);
         Bencode result = parser.parse();
-        System.out.println(result);
+
+        String url = result.dictionary.get("announce").toString();
+        System.out.println(url);
+
+
+        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        byte[] testHash = mDigest.digest(result.dictionary.get("info").toString().getBytes());
+        StringBuilder resultHash = new StringBuilder();
+        for (byte testHash1 : testHash) {
+            resultHash.append(Integer.toString((testHash1 & 0xff) + 0x100, 16).substring(1));
+        }
+        System.out.println(resultHash);
     }
+
 }
